7e83ccbef7005ef298c5d68af2adf5ffc99df652
Corwin Buzzard <corwin1998@gmail.com>
2023-02-25 21:43:07 -0500

    Added auth

--- main.go
+++ main.go
@@ -12,20 +12,24 @@ import (
 func main() {
 	port := os.Getenv("PORT")
 	API_KEY := os.Getenv("API_KEY")
+	AUTH_KEY := os.Getenv("AUTH_KEY")
 
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
 
-	remote, err := url.Parse("https://api.openai.com/v1/completions")
+	remote, err := url.Parse("https://api.openai.com")
 	if err != nil {
 		panic(err)
 	}
 
 	handler := func(p *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
 		return func(w http.ResponseWriter, r *http.Request) {
+			if r.Header.Get("Auth") != AUTH_KEY {
+				return
+			}
 			r.Host = remote.Host
-			w.Header().Set("Authorization", fmt.Sprintf("Bearer %v", API_KEY))
+			r.Header.Add("Authorization", fmt.Sprintf("Bearer %v", API_KEY))
 			p.ServeHTTP(w, r)
 		}
 	}
