63eb0065e6af8b1a6ae4491c262dad0de1dce197
Corwin Buzzard <corwin1998@gmail.com>
2023-04-09 15:02:18 -0400

    Added support for sse

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"net/http/httputil"
@@ -30,7 +31,11 @@ func main() {
 			}
 			r.Host = remote.Host
 			r.Header.Add("Authorization", fmt.Sprintf("Bearer %v", API_KEY))
-			p.ServeHTTP(w, r)
+			if r.Header.Get("Accept") == "text/event-stream" {
+				handleSSE(p, w, r)
+			} else {
+				p.ServeHTTP(w, r)
+			}
 		}
 	}
 
@@ -40,4 +45,32 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func handleSSE(proxy *httputil.ReverseProxy, w http.ResponseWriter, r *http.Request) {
+	proxy.Director(r)
+	resp, err := http.DefaultTransport.RoundTrip(r)
+	if err != nil {
+		http.Error(w, "Error contacting backend server", http.StatusInternalServerError)
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Copy headers
+	for k, vv := range resp.Header {
+		for _, v := range vv {
+			w.Header().Add(k, v)
+		}
+	}
+
+	// Set the headers related to SSE
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+
+	w.WriteHeader(resp.StatusCode)
+
+	// Stream the data
+	io.Copy(w, resp.Body)
 }
